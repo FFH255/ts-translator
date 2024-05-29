b6bb78106c8f9e76707a2a6e0a50ee588845237b
Vsevolod Fadeev <sevafad@gmail.com>
2024-05-29 23:57:17 +0300

    Create interpreter.

--- src/cli/main.ts
+++ src/cli/main.ts
@@ -1,8 +1,9 @@
 import { Lexer } from "../domain/lexer.ts"
 import { Parser } from "../domain/parser.ts"
 import { SourceCode } from "../domain/source-code.ts"
+import { Interpreter } from "../domain/interpreter.ts"
 
-const input = "аа1"
+const input = "1 + 1 * 5 / 1"
 
 const src = new SourceCode(input)
 
@@ -14,4 +15,8 @@ const parser = new Parser()
 
 const program = parser.parse(tokens)
 
-console.log(program)
+const interpreter = new Interpreter()
+
+const result = interpreter.interpret(program)
+
+console.log(tokens, "\n", program, "\n", result)
---
+++ src/domain/interpreter.ts
@@ -0,0 +1,85 @@
+import {
+  BinaryExpression,
+  NodeType,
+  NumericExpression,
+  Program,
+  Statement,
+} from "./ast.ts"
+
+const enum ValueType {
+  Number,
+}
+
+class RuntimeValue {
+  constructor(public type: ValueType) {}
+}
+
+class NumberValue extends RuntimeValue {
+  constructor(public value: number) {
+    super(ValueType.Number)
+  }
+}
+
+export class Interpreter {
+  private evaluateProgram(program: Program): RuntimeValue {
+    let last: RuntimeValue | null = null
+
+    for (const statement of program.boby) {
+      last = this.interpret(statement)
+    }
+
+    if (last === null) {
+      throw new Error()
+    }
+
+    return last
+  }
+
+  private evaluateNumericBinaryExpression(
+    lhs: NumberValue,
+    rhs: NumberValue,
+    operator: string
+  ): NumberValue {
+    let result: number
+    if (operator == "+") {
+      result = lhs.value + rhs.value
+    } else if (operator == "-") {
+      result = lhs.value - rhs.value
+    } else if (operator == "*") {
+      result = lhs.value * rhs.value
+    } else if (operator == "/") {
+      // TODO: Division by zero checks
+      result = lhs.value / rhs.value
+    } else {
+      result = lhs.value % rhs.value
+    }
+
+    return new NumberValue(result)
+  }
+
+  private evaluateBinaryExpression(exp: BinaryExpression): RuntimeValue {
+    const lhs = this.interpret(exp.left)
+    const rhs = this.interpret(exp.right)
+    if (lhs.type === ValueType.Number && rhs.type === ValueType.Number) {
+      return this.evaluateNumericBinaryExpression(
+        lhs as NumberValue,
+        rhs as NumberValue,
+        exp.operator
+      )
+    }
+    throw new Error()
+  }
+
+  interpret(astNode: Statement): RuntimeValue {
+    switch (astNode.kind) {
+      case NodeType.Numeric:
+        return new NumberValue((astNode as NumericExpression).value)
+      case NodeType.BinaryExpression:
+        return this.evaluateBinaryExpression(astNode as BinaryExpression)
+      case NodeType.Programm:
+        return this.evaluateProgram(astNode as Program)
+      default:
+        throw new Error()
+    }
+  }
+}
