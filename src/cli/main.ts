4d5a0d5b0581877047ffc01d040182daa1d42da0
Vsevolod Fadeev <sevafad@gmail.com>
2024-05-30 00:06:09 +0300

    Create cli program.

--- src/cli/main.ts
+++ src/cli/main.ts
@@ -1,22 +1,26 @@
+/* eslint-disable no-constant-condition */
 import { Lexer } from "../domain/lexer.ts"
 import { Parser } from "../domain/parser.ts"
 import { SourceCode } from "../domain/source-code.ts"
 import { Interpreter } from "../domain/interpreter.ts"
 
-const input = "1 + 1 * 5 / 1"
-
-const src = new SourceCode(input)
-
 const lexer = new Lexer()
-
-const tokens = lexer.tokenize(src)
-
 const parser = new Parser()
-
-const program = parser.parse(tokens)
-
 const interpreter = new Interpreter()
 
-const result = interpreter.interpret(program)
+while (true) {
+  const input = prompt("> ")
+  if (!input || input.includes("exit")) {
+    Deno.exit(1)
+  }
 
-console.log(tokens, "\n", program, "\n", result)
+  const src = new SourceCode(input)
+
+  const tokens = lexer.tokenize(src)
+
+  const program = parser.parse(tokens)
+
+  const result = interpreter.interpret(program)
+
+  console.log(tokens, "\n", program, "\n", result)
+}
